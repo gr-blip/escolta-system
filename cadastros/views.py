7cd753f5fb32ada9d0745bee3359b06bbeb32d0e
Wilker <gr@gruposmith.seg.br>
2026-04-13 19:44:22 -0300

    fix: adiciona import csrf_exempt no views.py

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from django.db.models import Q
 from django.http import JsonResponse
