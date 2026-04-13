e6306ecb48187c7003aec89c6d9f243080ff6249
Wilker <gr@gruposmith.seg.br>
2026-04-13 10:33:07 -0300

    Migration 0023: adiciona FK os em DespesaOS

---
+++ cadastros/migrations/0023_despesaos_os_fk.py
@@ -0,0 +1,21 @@
+﻿from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('cadastros', '0022_despesaos_trocamotorista_parada_incidente_and_more'),
+    ]
+    operations = [
+        migrations.AddField(
+            model_name='despesaos',
+            name='os',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='despesas',
+                to='cadastros.ordemservico',
+                verbose_name='OS',
+                default=1,
+            ),
+            preserve_default=False,
+        ),
+    ]
