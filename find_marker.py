74f1e5b53ca9c0e3cd7cad2ff55c5693f25764a8
Wilker <gr@gruposmith.seg.br>
2026-04-12 22:03:53 -0300

    chore: remove scripts de debug do repositorio

--- find_marker.py
+++
@@ -1,7 +0,0 @@
-﻿path = r'D:\Sistema Escolta\escolta_system\cadastros\views.py'
-content = open(path, encoding='utf-8').read()
-# Acha todas as ocorrencias
-import re
-for m in re.finditer(r"return JsonResponse\(\{'ok': True\}\)", content):
-    print(m.start(), repr(content[m.start()-50:m.start()+40]))
-    print('---')
--- fix_urls.py
+++
@@ -1,6 +0,0 @@
-﻿path = r'D:\Sistema Escolta\escolta_system\cadastros\urls.py'
-content = open(path, encoding='utf-8').read()
-content = content.replace("\nurlpatterns += [\n    _path('debug-media/', views.debug_media, name='debug_media'),\n]", '')
-content = content.replace("\nurlpatterns += [\n    _path('debug-op/', views.debug_op, name='debug_op'),\n]", '')
-open(path, 'w', encoding='utf-8').write(content)
-print('OK')
