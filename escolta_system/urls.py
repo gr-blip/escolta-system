b8ca03f84fa379c0f43b563b9c24c68c729a4b7d
Will <gr@gruposmith.seg.br>
2026-04-23 20:36:06 -0300

    deploy(4a): serve /media/ em prod + MEDIA_ROOT via env

--- escolta_system/urls.py
+++ escolta_system/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='cadastros/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('cadastros.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve /media/ em DEV e PROD.
+# Em DEV: helper static() padrão do Django.
+# Em PROD: django.views.static.serve (necessário pq Railway não tem nginx/S3 dedicado).
+#   Risco de perf/security é aceitável pro volume de uploads deste sistema.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
